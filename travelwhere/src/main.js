5755b989f919f00c6246b25108acb841973a4684
heyBlackSwan <liangtongyuan818@sina.cn>
2019-03-25 16:06:46 +0800

    0325 project init

--- travelwhere/src/main.js
+++ travelwhere/src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/reset.css'
+// 高倍屏一像素边框问题解决方案
+import './assets/styles/border.css'
+// 移动端的点击300毫秒延迟解决方案:安装 fastclick 到依赖中(npm install fastclick --save)
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
--- travelwhere/src/router/index.js
+++ travelwhere/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+// @对应的是src目录
+import Home from '@/pages/home/Home'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
